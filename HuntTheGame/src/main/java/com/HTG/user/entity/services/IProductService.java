ab1e679b79594dad3cbf4e2540aeaf12f31d54ae
Nthehunter <creeper1405@gmail.com>
2021-10-16 13:12:08 +0100

    database ok

--- HuntTheGame/src/main/java/com/HTG/user/entity/services/IProductService.java
+++
@@ -1,9 +0,0 @@
-package com.HTG.user.entity.services;
-
-import java.util.List;
-
-import com.HTG.user.entity.models.Product;
-
-public interface IProductService {
-	public List<Product> getAll();
-}
